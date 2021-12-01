218287733b8faa29d93a59340516b5b5059b1ad8
Robert <robert890501@mail.ru>
2021-12-01 15:24:41 +0300

    01.12.21

--- src/js/script.js
+++ src/js/script.js
@@ -38,3 +38,28 @@
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });
+
+  (function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.conteiner').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      
+    
+      function toggleSlede (item) {
+        $(item).each(function(i) {
+          $(this).on ('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+        });
+      }
+      toggleSlede ('.catalog-item__link');
+      toggleSlede ('.catalog-item__back');
+  });
+  })(jQuery);

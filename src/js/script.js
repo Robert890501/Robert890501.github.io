0ced932c139eb4dfff710677a5b84ece64da0506
Robert <robert890501@mail.ru>
2021-12-10 16:12:44 +0300

    lessons 65

--- src/js/script.js
+++ src/js/script.js
@@ -66,5 +66,71 @@
       $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
       });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');  
+      });
+
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        });
+      });
+
+      //Валидация
+      
+      function validateForms(form){
+        $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required:true,
+            email:true
+          }
+        },
+        messages: {
+          name: {
+            required: "Введите Ваше имя",
+            minlength: jQuery.validator.format("Минимум {0} символов!")
+          },
+          phone: "Введите номер телефона",
+          email: {
+            required: "Введите Ваш е-майл",
+            email: "Не правильный формат адреса"
+          }
+        }
+      });
+      }
+
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+      validateForms('#order form');
+
+      //маска телефона-удалить формат чисел y импут.
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      //отправка данных на почту
+      $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+          return;
+        }
+        $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+        }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+          $('form').trigger('reset');
+        });
+        return false;
+      });
+
   });
   })(jQuery);

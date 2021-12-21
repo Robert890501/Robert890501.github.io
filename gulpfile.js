17ccf8cf556e3700c3527219920566d11405b27b
Robert <robert890501@mail.ru>
2021-12-21 09:28:22 +0300

    maller

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task('styles', function() {
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
 });
 
 gulp.task('html', function () {
@@ -41,7 +42,8 @@ gulp.task('html', function () {
 
 gulp.task('scripts', function () {
     return gulp.src("src/js/**/*.js")
-        .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function () {
